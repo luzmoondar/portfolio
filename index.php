dce053fc6de0a67a2324ecff7d4373c7cbbddd99
luzmoondar <133784112+luzmoondar@users.noreply.github.com>
2023-12-31 01:49:57 +0900

    php실험

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="reset.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <link rel="stylesheet" href="https://use.typekit.net/htw4hev.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://webfontworld.github.io/sandbox/SBAggro.css" rel="stylesheet">
+    <title>오션빌글램핑</title>
+    <style>
+        footer{position:relative; width:100%; height:960px; background-image: url("https://github.com/luzmoondar/portfolio/assets/133784112/582e2d6d-6e89-460c-a2c4-90adc4d90cf0");}
+        .mainft{position:absolute; bottom:0; width:100%; background-color: rgba(0,0,0,0.5); border-top:1px solid #e5e4e4;}
+        .mainft div{width:80%; margin:0 auto; padding: 80px 0;}
+        footer h2{font-family: 'SBAggro'; font-weight:600; font-size:30px; letter-spacing: 1.2px; color:#e5e4e4; position: absolute; bottom:274px; left:190px;}
+        footer h3{font-size: 18px; font-family: "pretendard", sans-serif; color:#e5e4e4; margin-bottom:10px;}
+        footer p{font-size:14px; font-family: "pretendard", sans-serif; color:#e5e4e4; line-height: 22px; font-weight: 100;}
+        footer .mbr{display: none;}
+        @media (max-width: 768px) {
+            footer p .m_none{display:none;}
+        }
+    </style>
+</head>
+
+<body>
+    <?php 
+        include 'header.php';
+    ?>
+
+    <header>
+        <nav id="main-nav">
+            <div class="logo">
+                <a href="#">
+                    <img src="https://github.com/luzmoondar/portfolio/assets/133784112/48d3136c-fff9-4b66-9c22-74a640f101fd">
+                    <p>OCEANVILL<br>GLAMPING</p>
+                </a>
+            </div>
+            <div class="scroll-logo">
+                <div>
+                    <img src="https://github.com/luzmoondar/portfolio/assets/133784112/112349c2-64f6-43d1-a6a1-1b9d9a200514">
+                    <p>OCEANVILL GLAMPING</p>
+                </div>
+            </div>
+            <div class="manubar">
+                <ul class="main-manu">
+                    <li>
+                        <a href="landscape.html">ABOUT</a>
+                    </li>
+                    <li><a href="room1.html">ROOMS</a></li>
+                    <li><a href="reser.html">RESERVE</a></li>
+                    <li><a href="facility.html">FACILITY</a></li>
+                    <li><a class="reser-bt"><img class="reserimg" src="https://github.com/luzmoondar/portfolio/assets/133784112/2ab39636-f84e-4e5f-a764-0b7d822ae99b">실시간예약</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="sub-manu">
+            <div>
+                <ul>
+                    <li><a href="landscape.html">외부전경</a></li>
+                    <li><a href="">오시는 길</a></li>
+                    <li><a href="travel.html">주변여행지</a></li>
+                </ul>
+                <ul>
+                    <li><a>객실배치도</a></li>
+                    <li><a href="room1.html">101호</a></li>
+                    <li><a>102호</a></li>
+                    <li><a>103호</a></li>
+                    <li><a>104호</a></li>
+                    <li><a>105호</a></li>
+                    <li><a>106호</a></li>
+                    <li><a>107호</a></li>
+                </ul>
+                <ul>
+                    <li><a href="reser.html">종합예약안내</a></li>
+                    <li><a href="https://booking.pensionnara.co.kr/calendar/256" target="_">실시간예약</a></li>
+                </ul>
+                <ul>
+                    <li><a href="facility.html">대형수영장</a></li>
+                    <li><a href="facility.html">온수수영장</a></li>
+                    <li><a href="facility.html">개별바비큐</a></li>
+                    <li><a href="facility.html">오션뷰</a></li>
+                    <li><a href="facility.html">무선인터넷</a></li>
+                </ul>
+            </div>
+        </div> 
+    </header>
+    
+
+    <section>
+        <div class="topcon">
+            <div class="slide">
+                <img src="img/main/10.jpg">
+                <img src="img/main/10.jpg">
+            </div>
+            <div class="slide_button">
+                <div>
+                    <p>1</p>
+                    <p class="slide-num-line"></p>
+                    <p>20</p>
+                    <button class="prev"><img src="img/main/prev.png"></button>
+                    <button class="stop">STOP</button>
+                    <button class="next"><img src="img/main/next.png"></button>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    <section>
+        <div class="">
+            <div class="main_con">
+                <div class="mainab">
+                    <div class="left">
+                        <div>
+                            <img src="https://github.com/luzmoondar/portfolio/assets/133784112/7ac5ab21-d17a-4652-80d0-054f52d08c61">
+                        </div>
+                    </div>
+                    <div class="right">
+                    </div>
+                </div>
+                <div class="roompv">
+                    <div class="roombg">
+                        <div class="roompvtxt">
+                            <h2>ROOMS</h2>
+                            <p>
+                                잔잔한 파도 소리와 함께하는<br>
+                                힐링 여행을 즐겨보세요.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="roomslide">
+                        <a class="room1">
+                            <img src="https://github.com/luzmoondar/portfolio/assets/133784112/7d99c960-bddd-4890-a9f7-7f085e9490b0">
+                            <div class="roomname">
+                                <span></span><p>101</p>
+                            </div>
+                        </a>
+                        <a class="room1">
+                            <img src="https://github.com/luzmoondar/portfolio/assets/133784112/e8737c90-5ca4-401c-913c-cbe94129581f">
+                            <div class="roomname">
+                                <span></span><p>102</p>
+                            </div>
+                        </a>
+                        <a class="room1">
+                            <img src="https://github.com/luzmoondar/portfolio/assets/133784112/4e9d8852-0819-44da-ac06-e41babfb9af7">
+                            <div class="roomname">
+                                <span></span><p>103</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="mainsp">
+                    <div class="mainsptxt">
+                        <h2>FACILITY</h2>
+                        <p>
+                            즐길거리
+                        </p>
+                    </div>
+                    <div class="main_spslide">
+                        <a>
+                            <img src="https://github.com/luzmoondar/portfolio/assets/133784112/99fcfb23-d3a0-47f1-9f27-77c24f21ff22">
+                            <div class="spname">
+                                <p>large swimming pool</p><span></span>
+                            </div>
+                        </a>
+                        <a>
+                            <img src="https://github.com/luzmoondar/portfolio/assets/133784112/9a6bf849-5ea4-4dce-b26d-e572c8afbf24">
+                            <div class="spname">
+                                <p>outdoor swimming pool</p><span></span>
+                            </div>
+                        </a>
+                        <a>
+                            <img src="https://github.com/luzmoondar/portfolio/assets/133784112/5578de9e-afdf-4fd5-a2f0-95fa2bbe487f">
+                            <div class="spname">
+                                <p>individual barbecue place</p><span></span>
+                            </div>
+                        </a>
+                        <a>
+                            <img src="https://github.com/luzmoondar/portfolio/assets/133784112/6bf30c2c-f445-4214-992d-34be26891b6b">
+                            <div class="spname">
+                                <p>outdoor swimming pool</p><span></span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <h2>OCEANVILL GLAMPING</h2>
+        <div class="mainft">
+            <div>
+                <h3>오션빌글램핑</h3>
+                <p>
+                    TEL. 010-8362-7654<br>
+                    대표. 조인경 <br class="mbr"><span class="m_none">|</span> 인천광역시 강화군 화도면 해안남로 2421-227<br>
+                    사업자번호. 878-61-00796<br>
+                    ACC. 카카오뱅크 3333-11-1146663 (예금주 : 조인경)
+                </p>
+            </div>
+        </div>
+    </footer>
+    <script>
+        $(function(){
+            var fixScroll = 0;
+            $(window).scroll(function(event){
+                var scroll = $(this).scrollTop();
+                if (scroll > 100){
+                //스크롤 높이 설정
+                    $("#main-nav").addClass("active");
+                    $(".logo").css("display","none");
+                    $(".scroll-logo").css("display","block");
+                    $("nav .main-manu li a").css({"color":"#523a15","text-shadow":"none"});
+                    $(".reserimg").attr("src","https://github.com/luzmoondar/portfolio/assets/133784112/42b3cdd8-f83c-436b-86d0-ec7eb997bf09");
+                }
+                else {
+                    $("#main-nav").removeClass("active");
+                    $(".logo").css("display","block");
+                    $(".scroll-logo").css("display","none");
+                    $("nav .main-manu li a").css({"color":"#fff","text-shadow":"1px 1px 1px rgba(0,0,0,0.2)"});
+                    $(".reserimg").attr("src","https://github.com/luzmoondar/portfolio/assets/133784112/2ab39636-f84e-4e5f-a764-0b7d822ae99b");
+                }
+                fixScroll = scroll;
+            });
+        });
+        $(".main-manu li").mouseover(
+            function() {
+                $(".sub-manu").css("opacity","1");
+                $("#main-nav").addClass("active");
+                $(".logo").css("display","none");
+                $(".scroll-logo").css("display","block");
+                $("nav .main-manu li a").css({"color":"#523a15","text-shadow":"none"});
+                $(".reserimg").attr("src","https://github.com/luzmoondar/portfolio/assets/133784112/42b3cdd8-f83c-436b-86d0-ec7eb997bf09");
+            }
+        );
+        $(".sub-manu").mouseleave(
+            function() {
+                $(".sub-manu").css("opacity","0");
+            }
+        );
+        
+       </script>
+
+</body>
+
+</html>
